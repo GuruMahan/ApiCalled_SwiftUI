de74b5b2f6255035bd575ec0cd1c69f53a461e5b
GuruMahan <iosdeve2022@gmail.com>
2023-02-02 19:05:26 +0530

    Initial Commit

---
+++ ApiCalledSUI/ApiCalledSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCalledSUIApp.swift
+//  ApiCalledSUI
+//
+//  Created by Guru Mahan on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ApiCalledSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ApiCalledView()
+        }
+    }
+}
---
+++ ApiCalledSUI/ApiViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ApiViewModel.swift
+//  ApiCalledSUI
+//
+//  Created by Guru Mahan on 30/12/22.
+//
+
+import Foundation
+class ApiViewModel: ObservableObject {
+    @Published var isSelected:innerData?
+    @Published var dataList : ApiModel?
+    var link = "https://datausa.io/api/data?drilldowns=Nation&measures=Population"
+    
+    var jsonvalue = [innerData]()
+    
+    func loadData() async {
+        guard let url = URL(string: link) else{print("Invalid URL")
+            return
+        }
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(ApiModel.self, from: data){
+                DispatchQueue.main.async {
+                    self.jsonvalue = decodedResponse.data
+                    self.dataList = decodedResponse
+                    print(self.jsonvalue)
+                }
+                
+            }
+            
+        }catch{
+            print("Invalid data")
+        }
+    }
+    
+}
